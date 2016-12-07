f78c9d3cbe958d01c5ebe60ea22055b27739d1f5
dannynanni <dannynanni.us@gmail.com>
2016-12-07 11:35:38 -0500

    tap to play / click to play implemented

---
+++ Assets/Scripts/TapToPlay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TapToPlay : MonoBehaviour {
+
+	void Update () {
+	
+		if (Input.touchCount == 1) {
+
+			SceneManager.LoadScene ("balance", LoadSceneMode.Additive);
+		}
+
+	}
+
+	void OnMouseDown(){
+	
+		SceneManager.LoadScene ("balance", LoadSceneMode.Additive);
+	}
+}
