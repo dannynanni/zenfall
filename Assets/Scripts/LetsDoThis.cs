1e2d738170ab4630f3acf2d3fd0156093921bfb0
dannynanni <dannynanni.us@gmail.com>
2016-12-10 22:43:00 -0500

    adjusted input to overcome weirdness

--- Assets/Scripts/LetsDoThis.cs
+++ Assets/Scripts/LetsDoThis.cs
@@ -6,24 +6,34 @@ public class LetsDoThis : MonoBehaviour {
 
 	public Collider2D collider;
 
-	void Update () {
-
-		if (Input.touchCount == 1)
-		{
-			Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-			Vector2 touchPos = new Vector2(wp.x, wp.y);
-			if (collider == Physics2D.OverlapPoint(touchPos))
-			{
-				SceneManager.LoadScene ("balance", LoadSceneMode.Single);
-			}
-		}
-
+	void Start () {
+	
+		Invoke("LoadNext", 5);
 	}
+
+//	void Update () {
+//
+//		if (Input.touchCount == 1)
+//		{
+//			Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+//			Vector2 touchPos = new Vector2(wp.x, wp.y);
+//			if (collider == Physics2D.OverlapPoint(touchPos))
+//			{
+//				SceneManager.LoadScene ("balance", LoadSceneMode.Single);
+//			}
+//		}
+//
+//	}
 
 	void OnMouseDown(){
 
 		SceneManager.LoadScene ("balance", LoadSceneMode.Single);
 	}
 
+	void LoadNext(){
+
+		SceneManager.LoadScene ("balance", LoadSceneMode.Single);
+
+	}
 }
 
